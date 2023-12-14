2392ed393b0e0f4e5bafcb670d85b3f8bb26f0c2
kinwend17 <st.andrey17@yandex.ru>
2023-12-14 17:51:29 +0300

    some

--- ShopService/Program.cs
+++ ShopService/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.Extensions.Configuration;
+
 namespace ShopService
 {
-    internal static class Program
+    public static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
@@ -8,6 +10,14 @@ namespace ShopService
         [STAThread]
         static void Main()
         {
+            var cfg = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            // Если TRUE, то данные в SQLite, иначе InMemoryStream и CSV
+            bool? isSqlite = cfg.GetValue<bool>("StorageSettings:sqlite");
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
