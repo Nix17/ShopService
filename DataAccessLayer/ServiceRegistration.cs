d9672c21ff363d7027e57114c4c223be9307f449
Andrey Starostenkov <st.andrey17@yandex.ru>
2023-12-15 17:22:52 +0300

    some

--- DataAccessLayer/ServiceRegistration.cs
+++ DataAccessLayer/ServiceRegistration.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using DataAccessLayer.Contexts;
+using DataAccessLayer.Interfaces.Repositories;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +12,19 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer;
 
-internal class ServiceRegistration
+public static class ServiceRegistration
 {
+    public static void AddDataAccessLayerInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        //services.AddDbContext<ApplicationDbContext>(options =>        
+        //options.UseNpgsql(
+        //   configuration.GetConnectionString("DefaultConnection"),
+        //   b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
+        //            .UseSnakeCaseNamingConvention()
+        //    );
+        
+        
+        services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        services.AddTransient<IUnitOfWork, UnitOfWork>();
+    }
 }
